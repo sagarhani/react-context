d0f1eb2f98ea92613587d18005075b0476e262b0
sagarhani <sagarhani33@gmail.com>
2019-01-19 23:48:42 +0530

    Field and Button component

---
+++ src/components/Field.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Field extends React.Component {
+    render() {
+        return (
+            <div>
+                <label>Name</label>
+                <input />
+            </div>
+        );
+    }
+}
+
+export default Field;
